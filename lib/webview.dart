d412e64c215dd8a86eef298c0d64eba3f946f8c4
Sudin <sudean.mdrr@gmail.com>
2023-04-29 19:24:44 +0530

    need to push

--- lib/webview.dart
+++ lib/webview.dart
@@ -74,29 +74,7 @@ class WebviewClass extends StatelessWidget {
           },
         ),
       )
-      ..loadRequest(Uri.parse(
-          "https://m.uber.com/looking?drop%5B0%5D=%7B%22latitude%22%3A13.0353557%2C%22longitude%22%3A77.59878739999999%2C%22addressLine1%22%3A%22Hebbal%22%2C%22addressLine2%22%3A%22Bengaluru%2C%20Karnataka%22%2C%22id%22%3A%22ChIJRwrYlaIXrjsRWUexKPPLPBo%22%2C%22provider%22%3A%22google_places%22%2C%22index%22%3A0%7D&pickup=%7B%22latitude%22%3A12.9180865%2C%22longitude%22%3A77.6051008%2C%22addressLine1%22%3A%22BTM%201st%20Stage%22%2C%22addressLine2%22%3A%22BTM%20Layout%2C%20Bengaluru%2C%20Karnataka%22%2C%22id%22%3A%22ChIJSzfLnwEVrjsRaXJV_yIv08o%22%2C%22provider%22%3A%22google_places%22%2C%22index%22%3A0%7D&vehicle=2007"));
-
-    uberController
-        .setNavigationDelegate(NavigationDelegate(onPageFinished: (String url) {
-      uberController.runJavaScript('''getAutoValue = () => {
-      console.log('here');
-            const getAutoValue = () => {
-              return document.querySelector("#wrapper > div._css-dqxzrQ > div > div._css-fCssPB > div._css-bOmAYR > div > span > div > div._css-hWGanL > div > ul > li._css-dRVcpk > div._css-eemxAU > div._css-jhvuGu > div._css-bIstwm > div > p");
-            };
-            let intervalId;
-            intervalId = setInterval(() => {
-              const autoValue = getAutoValue();
-              if (autoValue) {
-                clearInterval(intervalId);
-                Uber.postMessage(autoValue.textContent);
-
-              }
-            }, 1000);
-          };
-      getAutoValue();
-      ''');
-    }));
+      ..loadRequest(Uri.parse("https://www.jotform.com/app/231091185886463"));
 
     controller
         .setNavigationDelegate(NavigationDelegate(onPageStarted: (String url) {
@@ -196,7 +174,7 @@ function sleepSync(ms) {
                 ),
               ),
               Visibility(
-                visible: cabDataController.showOlaWebView.value,
+                visible: false,
                 child: SizedBox(
                   height: 500,
                   child: WebViewWidget(
@@ -205,7 +183,7 @@ function sleepSync(ms) {
                 ),
               ),
               Visibility(
-                visible: cabDataController.showUberWebView.value,
+                visible: true,
                 child: SizedBox(
                   height: 500,
                   child: WebViewWidget(
